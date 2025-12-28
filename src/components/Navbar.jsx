c4f7d981285e3d22ad3e3868fe42647e30c60f39
Michael <chukwukeremichael25@gmail.com>
2025-12-28 10:59:35 -0800

    added

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,107 +1,153 @@
 import { Link, useNavigate } from "react-router-dom";
+import { useRef } from "react";
 import logo from "../assets/logo1.png";
 
 function Navbar() {
   const navigate = useNavigate();
+  const navRef = useRef();
+
   const isStudent = localStorage.getItem("studentAuth");
   const isAdmin = localStorage.getItem("adminAuth");
+
+  const closeNavbar = () => {
+    if (navRef.current?.classList.contains("show")) {
+      navRef.current.classList.remove("show");
+    }
+  };
 
   const logout = () => {
     localStorage.clear();
     navigate("/");
+    closeNavbar();
   };
 
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-3 sticky-top">
+    <nav className="navbar navbar-expand-lg navbar-dark bg-dark py-3 sticky-top shadow">
       <div className="container">
 
-        {/* LOGO + SCHOOL NAME */}
-        <Link className="navbar-brand d-flex align-items-center" to="/">
+        {/* ===== LOGO + SCHOOL NAME ===== */}
+        <Link
+          className="navbar-brand d-flex align-items-center"
+          to="/"
+          onClick={closeNavbar}
+        >
           <img
             src={logo}
             alt="UAES Logo"
             width="70"
+            height="70"
             className="me-2"
           />
-          <span className="fw-bold d-none d-md-block">
-            UNIVERSITY OF AGRICULTURE & <br />
-            ENVIRONMENTAL SCIENCES <br />
-            UMUAGWO
-          </span>
 
-          <span className="fw-bold d-block d-md-none">
+          {/* MOBILE NAME */}
+          <span className="fw-bold text-white d-block d-md-none">
             UAES UMUAGWO
           </span>
 
+          {/* DESKTOP NAME */}
+          <span className="fw-bold text-white d-none d-md-block">
+            UNIVERSITY OF AGRICULTURE & <br />
+            ENVIRONMENTAL SCIENCE UMUAGWO
+          </span>
         </Link>
 
-        {/* TOGGLER BUTTON (THIS WAS MISSING) */}
+        {/* ===== TOGGLER ===== */}
         <button
           className="navbar-toggler"
           type="button"
           data-bs-toggle="collapse"
-          data-bs-target="#mainNavbar"
-          aria-controls="mainNavbar"
+          data-bs-target="#navbarNav"
+          aria-controls="navbarNav"
           aria-expanded="false"
           aria-label="Toggle navigation"
         >
           <span className="navbar-toggler-icon"></span>
         </button>
 
-        {/* COLLAPSIBLE MENU */}
-        <div className="collapse navbar-collapse" id="mainNavbar">
+        {/* ===== COLLAPSIBLE MENU ===== */}
+        <div className="collapse navbar-collapse" id="navbarNav" ref={navRef}>
           <ul className="navbar-nav ms-auto align-items-lg-center">
 
+            {/* COMMON LINKS */}
             <li className="nav-item">
-              <Link className="nav-link" to="/">Home</Link>
+              <Link className="nav-link" to="/" onClick={closeNavbar}>
+                Home
+              </Link>
             </li>
 
             <li className="nav-item">
-              <Link className="nav-link" to="/results">Results</Link>
+              <Link className="nav-link" to="/results" onClick={closeNavbar}>
+                Results
+              </Link>
             </li>
 
-            {/* NOT LOGGED IN */}
+            {/* ===== NOT LOGGED IN ===== */}
             {!isStudent && !isAdmin && (
               <>
-                <li className="nav-item ms-lg-3 mt-2 mt-lg-0">
-                  <Link className="btn btn-success w-100" to="/login">
+                <li className="nav-item mt-2 mt-lg-0 ms-lg-3">
+                  <Link
+                    className="btn btn-success w-100"
+                    to="/login"
+                    onClick={closeNavbar}
+                  >
                     Student Login
                   </Link>
                 </li>
-                <li className="nav-item ms-lg-2 mt-2 mt-lg-0">
-                  <Link className="btn btn-outline-light w-100" to="/admin-login">
+
+                <li className="nav-item mt-2 mt-lg-0 ms-lg-2">
+                  <Link
+                    className="btn btn-outline-light w-100"
+                    to="/admin-login"
+                    onClick={closeNavbar}
+                  >
                     Admin Login
                   </Link>
                 </li>
               </>
             )}
 
-            {/* STUDENT */}
+            {/* ===== STUDENT LOGGED IN ===== */}
             {isStudent && (
               <>
-                <li className="nav-item ms-lg-3 mt-2 mt-lg-0">
-                  <Link className="btn btn-success w-100" to="/dashboard">
+                <li className="nav-item mt-2 mt-lg-0 ms-lg-3">
+                  <Link
+                    className="btn btn-success w-100"
+                    to="/dashboard"
+                    onClick={closeNavbar}
+                  >
                     Dashboard
                   </Link>
                 </li>
-                <li className="nav-item ms-lg-2 mt-2 mt-lg-0">
-                  <button className="btn btn-danger w-100" onClick={logout}>
+
+                <li className="nav-item mt-2 mt-lg-0 ms-lg-2">
+                  <button
+                    className="btn btn-danger w-100"
+                    onClick={logout}
+                  >
                     Logout
                   </button>
                 </li>
               </>
             )}
 
-            {/* ADMIN */}
+            {/* ===== ADMIN LOGGED IN ===== */}
             {isAdmin && (
               <>
-                <li className="nav-item ms-lg-3 mt-2 mt-lg-0">
-                  <Link className="btn btn-primary w-100" to="/admin-dashboard">
+                <li className="nav-item mt-2 mt-lg-0 ms-lg-3">
+                  <Link
+                    className="btn btn-primary w-100"
+                    to="/admin-dashboard"
+                    onClick={closeNavbar}
+                  >
                     Admin Panel
                   </Link>
                 </li>
-                <li className="nav-item ms-lg-2 mt-2 mt-lg-0">
-                  <button className="btn btn-danger w-100" onClick={logout}>
+
+                <li className="nav-item mt-2 mt-lg-0 ms-lg-2">
+                  <button
+                    className="btn btn-danger w-100"
+                    onClick={logout}
+                  >
                     Logout
                   </button>
                 </li>
@@ -110,7 +156,6 @@ function Navbar() {
 
           </ul>
         </div>
-
       </div>
     </nav>
   );
